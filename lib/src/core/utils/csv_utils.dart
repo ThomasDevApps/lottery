7186c4ae1aead4fc159d4f0a46abc5ed459264c8
Thomas FERREIRA <thomas.devapps@gmail.com>
2024-04-14 13:13:08 +0200

    fix

--- lib/src/core/utils/csv_utils.dart
+++ lib/src/core/utils/csv_utils.dart
@@ -5,21 +5,13 @@ class _CsvUtils {
     String path, {
     required bool fromAssets,
   }) async {
-    if (fromAssets) {
-      // Retrieve the file
-      final input = await rootBundle.loadString(path);
-      // Decode all fields
-      List<List<dynamic>> fields = const CsvToListConverter().convert(input);
-      return fields;
-    } else {
-      // Retrieve the file
-      final input = File(path).openRead();
-      // Decode all fields
-      List<List<dynamic>> fields = await input
-          .transform(utf8.decoder)
-          .transform(const CsvToListConverter())
-          .toList();
-      return fields;
-    }
+    // Retrieve the file
+    final input = File(path).openRead();
+    // Decode all fields
+    List<List<dynamic>> fields = await input
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
+    return fields;
   }
 }
